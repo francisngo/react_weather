7ee248d74e43f6da838bd8c407ed830b54f80b1e
francisngo <francis.t.ngo@gmail.com>
2017-02-07 10:38:39 -0800

    update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -7,7 +7,7 @@ var About = () => {
         <h1 className="text-center page-title">About</h1>
       </div>
       <div className="card-section">
-        <p>The WeatherCast is a simple weather application built with <a href="http://facebook.github.io/react">ReactJS</a> and <a href="http://openweathermap.org">OpenWeatherMap API</a>.</p>
+        <p>The WeatherCast is a simple weather application built with <a href="http://facebook.github.io/react" target="_blank">ReactJS</a>, <a href="http://openweathermap.org">OpenWeatherMap API</a>, styled with <a href="http://sass-lang.com/" target="_blank">Sass</a>, tested with <a href="https://karma-runner.github.io/" target="_blank">Karma</a>.</p>
         <p>Get today's current weather. To use the application, just search for a city (U.S. cities only).</p>
       </div>
     </div>
